0b7b6ad9ac88ecc23677fccf062c7ecf21456fd3
johnnywedl <johnnywedl@gmail.com>
2024-05-07 07:21:50 -0500

    Preloader and Button Hover interaction complete
    I was playing around with the code for a while, trying to figure out why the hover interaction wasn't working and found that the JQuery link was placed after the call to the js folder...apparently the link needs to go before. Nonetheless, that problem was fixed and the hover now works. Future iterations should include centering the text on the button. I also managed to fix syntax errors that allow for the preloader to bounce around for about 3000ms and move onto the main content.

--- js/index.js
+++ js/index.js
@@ -5,3 +5,16 @@ function fade() {
     $('.preloader').fadeOut("slow");
     }
     setTimeout(fade, 3000);  
+
+
+// Button hover
+
+$(document).ready(function(){
+    $('.button').mouseenter(function(){
+        $(this).css({"width": "120px","height":"40px"});
+    });
+
+    $('.button').mouseleave(function(){
+        $(this).css({"width" : "auto" , "height" : "auto"});
+    });
+})
